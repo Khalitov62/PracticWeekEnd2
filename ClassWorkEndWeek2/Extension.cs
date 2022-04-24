fc198695b83a773217b78f0e2a410045ba5ae41f
Islam Khalitov <tu7m5rk73@aztu.local>
2022-04-24 13:50:49 +0400

    added

---
+++ ClassWorkEndWeek2/Extension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassWorkEndWeek2
+{
+   static class Extension
+    {
+        public static stron Capitalized(this string word)
+        {
+            if (char.ToUpper(word[0]))
+            {
+                for (int i = 1; i < word.Length; i++)
+                {
+                    if (!char.IsLower(word[i]))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ ClassWorkEndWeek2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassWorkEndWeek2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "islam";
+            name.Capitalized();
+        }
+    }
+}
